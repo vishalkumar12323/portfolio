22d569c2fe8059eabb48c79ebd59a5f456e2725a
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-01-17 13:56:06 +0530

    update prisma schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ import { Archivo_Black } from "next/font/google"
 
 
 const archivo_black = Archivo_Black({
-  weight: "400"
+  weight: "400",
+  subsets: ['latin']
 });
 
 const Profile = lazy(() => import("@/components/main/profile"));
@@ -27,7 +28,7 @@ export default function HeroSectionPage() {
         <div className="md:max-w-[600px] flex-1 text-center md:text-left">
           <p className="text-xl text-muted-foreground mb-2">Hello 👋</p>
           <h1
-          style={{zoom: 1.4}}
+            style={{ zoom: 1.4 }}
             className={
               `${archivo_black.className} text-2xl sm:text-3xl md:text-4xl font-[900] px-0 py-4 bg-clip-text text-transparent dark:bg-gradient-to-b dark:from-white dark:to-neutral-400 bg-gradient-to-b from-black to-neutral-800`
             }
