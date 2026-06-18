0e057e179796e8fa9e0df100bc28e7b21fc24212
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-18 17:03:03 +0530

    update nextconfigs add PrismaPlugin support

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/*": ["./generated/prisma/*.node"],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...(config.plugins || []), new PrismaPlugin()];
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
