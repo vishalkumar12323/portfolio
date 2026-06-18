dff8f088323ba571c68cff8547cfd4312808b404
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-18 16:40:36 +0530

    include prisma engine binary in the serverless bundle.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/*": ["./generated/prisma/*.node"],
+  },
 };
 
 export default nextConfig;
