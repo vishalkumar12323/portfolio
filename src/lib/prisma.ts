2d69ea13dc7bb7893acc0b9ecdb79418b82bab0d
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-18 16:29:34 +0530

    Edit prisma.ts for prisma-client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
 import "dotenv/config"
 
 const globalForPrisma = global as unknown as {
