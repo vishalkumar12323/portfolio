8cc88531576d6ba5631ea0397322c3778fd2154b
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-30 21:34:29 +0530

    Replace prisma ORM with mongoose.

--- src/app/api/cloude-projects/route.ts
+++ src/app/api/cloude-projects/route.ts
@@ -1,14 +1,7 @@
 import { NextResponse } from "next/server";
-// import { prisma } from "@/lib/prisma";
 
 export const GET = async () => {
-    // try {
-    //     const projects = await prisma.projects.findMany({ include: { stats: { select: { commitMessage: true, timestamp: true } } } });
-    //     if (!projects || projects.length <= 0) return NextResponse.json({ message: "You have no any project on mongodb cloud" }, { status: 200 });
 
     return NextResponse.json({ "msg": "ok" }, { status: 200 });
-    // } catch (error) {
-    //     console.error("error fetching cloude projects:: ", error);
-    //     return NextResponse.json({ message: "Internal server error" }, { status: 500 });
-    // }
+
 };
