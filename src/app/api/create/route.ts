4db89051f12e353cbaa3fb658417da568a520b1a
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-30 21:34:55 +0530

    Replace prisma ORM with mongoose.

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-// import { prisma } from "@/lib/prisma";
 
 export const GET = () => {
     return NextResponse.json({ message: "ok" }, { status: 200 });
